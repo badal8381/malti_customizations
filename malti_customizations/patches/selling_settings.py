310d7a4eea445c3245c6b8b86572b4e022f8e88c
badal8381 <badalsahani8381@gmail.com>
2024-03-30 13:38:33 +0530

    feat: customizations in setting page using patches

---
+++ malti_customizations/patches/selling_settings.py
@@ -0,0 +1,13 @@
+import frappe
+
+
+def change_display_depends_on(doctype, fieldname, depends_on_condition):
+    docfield = frappe.get_doc("DocField", {"parent": doctype, "fieldname": fieldname})
+    if docfield:
+        docfield.depends_on = depends_on_condition
+        docfield.save()
+
+
+def execute():
+    change_display_depends_on("Selling Settings", "maintain_same_rate_action", "validate_selling_price")
+    change_display_depends_on("Selling Settings", "role_to_override_stop_action", "eval: doc.validate_selling_price && doc.maintain_same_rate_action == 'Stop'")
