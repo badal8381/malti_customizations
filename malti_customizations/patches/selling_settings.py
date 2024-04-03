b17efb5cdc81994962f65016cf3a70ab5cbdfb90
badal8381 <badalsahani8381@gmail.com>
2024-04-03 11:00:31 +0530

    fix: add server scripts and patches

--- malti_customizations/patches/selling_settings.py
+++ malti_customizations/patches/selling_settings.py
@@ -9,5 +9,5 @@ def change_display_depends_on(doctype, fieldname, depends_on_condition):
 
 
 def execute():
-    change_display_depends_on("Selling Settings", "maintain_same_rate_action", "validate_selling_price")
-    change_display_depends_on("Selling Settings", "role_to_override_stop_action", "eval: doc.validate_selling_price && doc.maintain_same_rate_action == 'Stop'")
+    change_display_depends_on("Selling Settings", "maintain_same_rate_action", "eval: (doc.validate_selling_price || doc.maintain_same_sales_rate)")
+    change_display_depends_on("Selling Settings", "role_to_override_stop_action", "eval: ((doc.validate_selling_price || doc.maintain_same_sales_rate) && doc.maintain_same_rate_action == 'Stop')")
